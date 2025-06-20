3391f971ba37e5a90fcbd5f1b07cb03ef585e236
DylanRothbauer <djrothbauer@gmail.com>
2025-06-19 20:52:28 -0500

    Updates to nav layout

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -22,23 +22,61 @@ import { useRouter } from 'vue-router'
     <div class="flex nav-container">
       <nav class="flex flex-col w-48">
         <div>
-          <h1 class="text-2xl">Funkollection</h1>
+          <h1 class="playfair-display-hometitle nav-head font-bold text-3xl flex-1 items-center">
+            Funkollection
+          </h1>
         </div>
-        <RouterLink to="/dashboard" class="nav-link text-xl" active-class="router-link-active"
-          >Dashboard</RouterLink
-        >
-        <RouterLink to="/collection" class="nav-link text-xl" active-class="router-link-active"
-          >Collection</RouterLink
-        >
-        <RouterLink to="/about" class="nav-link text-xl" active-class="router-link-active"
-          >About</RouterLink
-        >
+        <div class="margin-vertical">
+          <RouterLink
+            to="/dashboard"
+            class="flex nav-link text-xl items-center"
+            active-class="router-link-active"
+          >
+            <i class="pi pi-th-large pr-3 w-3 nav-icon"></i>
+            <span class="ml-2 flex-1">Dashboard</span>
+          </RouterLink>
+        </div>
+        <div class="margin-vertical">
+          <RouterLink
+            to="/collection"
+            class="flex nav-link text-xl items-center"
+            active-class="router-link-active"
+          >
+            <i class="pi pi-box pr-3 w-3 nav-icon"></i>
+            <span class="ml-2 flex-1">Collection</span>
+          </RouterLink>
+        </div>
+
+        <div class="margin-vertical">
+          <RouterLink
+            to="/add-pop"
+            class="flex nav-link text-xl items-center"
+            active-class="router-link-active"
+          >
+            <i class="pi pi-plus-circle pr-3 w-3 nav-icon"></i>
+            <span class="ml-2 flex-1">Add Pop</span>
+          </RouterLink>
+        </div>
+
+        <div class="margin-vertical">
+          <RouterLink
+            to="/about"
+            class="flex nav-link text-xl items-center"
+            active-class="router-link-active"
+          >
+            <i class="pi pi-box pr-3 w-3 nav-icon"></i>
+            <span class="ml-2 flex-1">About</span>
+          </RouterLink>
+        </div>
+
         <!-- Add more links as needed here -->
 
         <!-- Spacer pushes the button down-->
         <div class="flex-1"></div>
 
-        <button class="nav-link">Sign Out</button>
+        <div>
+          <button class="flex-1 nav-link">Sign Out</button>
+        </div>
       </nav>
     </div>
 
@@ -50,7 +88,8 @@ import { useRouter } from 'vue-router'
 
 <style>
 .nav-container {
-  padding-right: 3rem;
+  padding-right: 2rem;
+  padding-left: 2rem;
   text-align: center;
   color: var(--funkollection-soft-white);
   background-color: var(--funkollection-primary);
@@ -64,13 +103,27 @@ import { useRouter } from 'vue-router'
   cursor: pointer;
 }
 
-.router-link-active {
+.margin-vertical:has(.router-link-active) {
   background-color: var(--funkollection-secondary);
-  font-weight: bold;
+  font: bold;
   border-radius: 0.375rem; /* rounded-md */
 }
 
 .main-content {
   background-color: var(--funkollection-background);
+}
+
+.margin-vertical {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+
+.nav-head {
+  padding-top: 2rem;
+  padding-bottom: 4rem;
+}
+
+.nav-icon {
+  font-size: 2rem !important;
 }
 </style>
