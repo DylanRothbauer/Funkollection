906891d075fbcd2fb5024e86fb4978aa28ff8922
DylanRothbauer <djrothbauer@gmail.com>
2025-06-23 20:08:04 -0500

    Table improvments

--- src/components/CollectionTable.vue
+++ src/components/CollectionTable.vue
@@ -3,9 +3,16 @@ import { onMounted, ref } from 'vue'
 import { collection, getDocs, doc, getDoc } from 'firebase/firestore'
 import { db } from '@/firebase'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { FilterMatchMode } from '@primevue/core/api'
+import { InputText } from 'primevue'
+import { IconField } from 'primevue'
+import { InputIcon } from 'primevue'
+import Toolbar from 'primevue/toolbar'
+import Button from 'primevue/button'
 
 const funkos = ref([])
 const user = ref(null)
+const selectedFunkos = ref([])
 
 onMounted(() => {
   const auth = getAuth()
@@ -28,27 +35,158 @@ onMounted(() => {
     }
   })
 })
+
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  id: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  title: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  series: { value: null, matchMode: FilterMatchMode.CONTAINS },
+})
+const loading = ref(true)
+const dt = ref()
+
+onMounted(() => {
+  // ...your existing Firestore fetch logic...
+  loading.value = false
+})
+
+const exportCSV = () => {
+  dt.value.exportCSV()
+}
+const openNew = () => {
+  // Open your add dialog (implement as needed)
+}
+const editFunko = (funko) => {
+  // Open your edit dialog (implement as needed)
+}
+const confirmDeleteFunko = (funko) => {
+  // Open your delete dialog (implement as needed)
+}
+const confirmDeleteSelected = () => {
+  // Open your bulk delete dialog (implement as needed)
+}
 </script>
 
 <template>
-  <div class="card">
-    <DataTable
-      :value="funkos"
-      paginator
-      :rows="5"
-      :rowsPerPageOptions="[5, 10, 20, 50]"
-      tableStyle="min-width: 50rem"
-    >
-      <Column field="id" header="ID" style="width: 25%"></Column>
-      <Column field="name" header="Name" style="width: 25%"></Column>
-      <Column field="title" header="Title" style="width: 25%"></Column>
-      <Column field="series" header="Series" style="width: 25%"></Column>
-    </DataTable>
+  <div>
+    <div class="card">
+      <Toolbar class="mb-6">
+        <template #start>
+          <Button label="New" icon="pi pi-plus" class="mr-2" @click="openNew" />
+          <Button
+            label="Delete"
+            icon="pi pi-trash"
+            severity="danger"
+            outlined
+            @click="confirmDeleteSelected"
+            :disabled="!selectedFunkos || !selectedFunkos.length"
+          />
+        </template>
+        <template #end>
+          <Button
+            label="Export"
+            icon="pi pi-upload"
+            severity="secondary"
+            @click="exportCSV($event)"
+          />
+        </template>
+      </Toolbar>
+
+      <DataTable
+        ref="dt"
+        v-model:selection="selectedFunkos"
+        :value="funkos"
+        dataKey="id"
+        :paginator="true"
+        :rows="10"
+        :filters="filters"
+        filterDisplay="row"
+        :globalFilterFields="['id', 'name', 'title', 'series']"
+        tableStyle="min-width: 50rem"
+      >
+        <template #header>
+          <div class="flex flex-wrap gap-2 items-center justify-between">
+            <h4 class="m-0">My Funko Pops</h4>
+            <IconField>
+              <InputIcon>
+                <i class="pi pi-search" />
+              </InputIcon>
+              <InputText v-model="filters['global'].value" placeholder="Search..." />
+            </IconField>
+          </div>
+        </template>
+        <template #empty> No Funko Pops found. </template>
+        <template #loading> Loading Funko Pops data. Please wait. </template>
+
+        <Column selectionMode="multiple" style="width: 3rem" :exportable="false"></Column>
+        <Column field="id" header="ID" sortable style="min-width: 10rem">
+          <template #filter="{ filterModel, filterCallback }">
+            <InputText
+              v-model="filterModel.value"
+              @input="filterCallback()"
+              placeholder="Search by ID"
+            />
+          </template>
+        </Column>
+        <Column field="name" header="Name" sortable style="min-width: 12rem">
+          <template #filter="{ filterModel, filterCallback }">
+            <InputText
+              v-model="filterModel.value"
+              @input="filterCallback()"
+              placeholder="Search by name"
+            />
+          </template>
+        </Column>
+        <Column field="title" header="Title" sortable style="min-width: 12rem">
+          <template #filter="{ filterModel, filterCallback }">
+            <InputText
+              v-model="filterModel.value"
+              @input="filterCallback()"
+              placeholder="Search by title"
+            />
+          </template>
+        </Column>
+        <Column field="series" header="Series" sortable style="min-width: 12rem">
+          <template #filter="{ filterModel, filterCallback }">
+            <InputText
+              v-model="filterModel.value"
+              @input="filterCallback()"
+              placeholder="Search by series"
+            />
+          </template>
+        </Column>
+        <Column :exportable="false" style="min-width: 8rem">
+          <template #body="slotProps">
+            <Button
+              icon="pi pi-pencil"
+              outlined
+              rounded
+              class="mr-2"
+              @click="editFunko(slotProps.data)"
+            />
+            <Button
+              icon="pi pi-trash"
+              outlined
+              rounded
+              severity="danger"
+              @click="confirmDeleteFunko(slotProps.data)"
+            />
+          </template>
+        </Column>
+      </DataTable>
+    </div>
+    <!-- Dialogs for add/edit/delete can be added here -->
   </div>
 </template>
 
 <style>
 .p-datatable-header-cell {
-  background-color: aqua !important;
+  background-color: var(--funkollection-secondary) !important;
+  color: var(--funkollection-soft-white) !important;
+}
+
+.p-datatable-header {
+  background: var(--funkollection-background) !important;
 }
 </style>
--- src/views/AddPop.vue
+++ src/views/AddPop.vue
@@ -5,6 +5,136 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { collection, addDoc } from 'firebase/firestore'
 import { doc, getDoc, setDoc } from 'firebase/firestore'
 
+const seriesOptions = [
+  'Pop! 8-Bit',
+  'Pop! Ad Icons',
+  'Pop! Air Force',
+  'Pop! Albums',
+  'Pop! Animation',
+  'Pop! Aquasox',
+  'Pop! Army',
+  'Pop! Around the World',
+  'Pop! Artists',
+  'Pop! Art Covers',
+  'Pop! Art Series',
+  'Pop! Asia',
+  'Pop! Bape',
+  'Pop! Basketball',
+  'Pop! Board Games',
+  'Pop! Books',
+  'Pop! Boxing',
+  'Pop! Broadway',
+  'Pop! Build a Bear',
+  'Pop! Candy',
+  'Pop! Christmas',
+  'Pop! Classics',
+  'Pop! College',
+  'Pop! Comedians',
+  'Pop! Comic Covers',
+  'Pop! Comics',
+  'Pop! Conan',
+  'Pop! Custom',
+  'Pop! Deluxe',
+  'Pop! Deluxe Moments',
+  'Pop! Die-Cast',
+  'Pop! Digital',
+  'Pop! Disney',
+  'Pop! Directors',
+  'Pop! Drag Queens',
+  'Pop! Fantastic Beasts',
+  'Pop! Fashion',
+  'Pop! Foodies',
+  'Pop! Football',
+  'Pop! Funko (Freddy Funko)',
+  'Pop! Funko (Fantastik Plastik)',
+  'Pop! Funko (Lance)',
+  'Pop! Game of Thrones',
+  'Pop! Games',
+  'Pop! Game Covers',
+  'Pop! Golf',
+  'Pop! GPK',
+  'Pop! Halo',
+  'Pop! Harry Potter',
+  'Pop! Heroes',
+  'Pop! Hockey',
+  'Pop! Holidays',
+  'Pop! House of the Dragons',
+  'Pop! Icons',
+  'Pop! League of Legends',
+  'Pop! Magic: The Gathering',
+  'Pop! Marines',
+  'Pop! Marvel',
+  'Pop! Magazine Covers',
+  'Pop! Minis',
+  'Pop! MLB',
+  'Pop! Moments',
+  'Pop! Monsters',
+  'Pop! Movie Posters',
+  'Pop! Movies',
+  'Pop! Muppets',
+  'Pop! Myths',
+  'Pop! My Little Pony',
+  'Pop! NASCAR',
+  'Pop! Navy',
+  'Pop! NBA Mascots',
+  'Pop! NFL',
+  'Pop! Pets',
+  'Pop! Pusheen',
+  'Pop! Racing',
+  'Pop! Retro Toys',
+  'Pop! Rides',
+  'Pop! Rocks',
+  'Pop! Royals',
+  'Pop! Sanrio',
+  'Pop! Sci-Fi',
+  'Pop! Sesame Street',
+  'Pop! SNL',
+  'Pop! South Park',
+  'Special Edition Pop!',
+  'Pop! Sports',
+  'Pop! Sports Legends',
+  'Pop! Stan Lee',
+  'Pop! Star Wars',
+  'Pop! Television',
+  'Pop! Tennis',
+  'Pop! The Vote',
+  'Pop! Town',
+  'Pop! Town Christmas',
+  'Pop! Trading Cards',
+  'Pop! Trains',
+  'Pop! Trolls',
+  'Pop! UFC',
+  'Pop! Uglydoll',
+  'Pop! Valiant',
+  'Pop! Vans',
+  'Pop! VHS Covers',
+  'Pop! Wreck-It Ralph',
+  'Pop! Wrestling',
+  'Pop! WWE',
+  'Pop! WWE Covers',
+  'Pop! Zodiac',
+]
+
+const funkoImage = ref('')
+
+const handleImageUpload = (event) => {
+  const file = event.target.files[0]
+  if (file) {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      funkoImage.value = e.target.result
+    }
+    reader.readAsDataURL(file)
+  }
+}
+const seriesSuggestions = ref([])
+
+const search = (event) => {
+  const query = event.query.toLowerCase()
+  seriesSuggestions.value = seriesOptions.filter((option) => option.toLowerCase().includes(query))
+}
+
+const selectedSeries = ref('')
 const user = ref(null)
 const funkoName = ref('')
 const funkoTitle = ref('')
@@ -30,7 +160,7 @@ const addFunkoPop = async () => {
       await setDoc(funkoDocRef, {
         name: funkoName.value,
         title: funkoTitle.value,
-        series: funkoSeries.value,
+        series: selectedSeries.value?.value || '',
         id: funkoID.value,
         createdAt: new Date(),
       })
@@ -57,6 +187,7 @@ const addFunkoPop = async () => {
     funkoName.value = ''
     funkoTitle.value = ''
     funkoSeries.value = ''
+    selectedSeries.value = ''
     funkoID.value = ''
 
     success.value = 'Funko Pop added!'
@@ -74,7 +205,15 @@ const addFunkoPop = async () => {
     <form @submit.prevent="addFunkoPop" class="flex flex-col gap-4">
       <input v-model="funkoName" placeholder="Name" class="p-2 border rounded" required />
       <input v-model="funkoTitle" placeholder="Title" class="p-2 border rounded" required />
-      <input v-model="funkoSeries" placeholder="Series" class="p-2 border rounded" />
+      <AutoComplete
+        v-model="selectedSeries"
+        :suggestions="seriesSuggestions"
+        :dropdown="true"
+        @complete="search"
+        placeholder="Select or type series"
+        required
+        class="dropdown-select"
+      />
       <input v-model="funkoID" placeholder="ID" class="p-2 border rounded" required />
       <button type="submit" class="p-2 bg-blue-500 text-white rounded">Add Funko Pop</button>
       <div v-if="success" class="text-green-600">{{ success }}</div>
@@ -82,3 +221,5 @@ const addFunkoPop = async () => {
     </form>
   </div>
 </template>
+
+<style></style>
