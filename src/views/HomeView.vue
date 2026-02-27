a1ef53b77280daf9f09c46b753c25966faef3c7c
Dylan Rothbauer <djrothbauer@gmail.com>
2026-02-27 14:55:32 -0600

    better grid & images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -72,8 +72,11 @@ const faqs = [
   </header>
 
   <main class="main-div w-screen h-screen items-center justify-center">
-    <div class="grid grid-cols-6 gap-8 place-content-center h-full">
-      <div class="grid w-full h-full col-span-3 col-start-1 pr-12">
+    <!-- Hero Section -->
+    <div class="grid grid-col-1 md:grid-cols-6 gap-8 place-content-center py-16 md:py-32">
+
+      <!-- Left -->
+      <div class="grid w-full col-span-6 md:col-span-3 md:col-start-1 md:pr-12">
         <div>
           <h1 class="playfair-display-hometitle text-7xl landing-title font-bold">Collect.</h1>
           <h1 class="playfair-display-hometitle text-7xl landing-title font-bold">Organize.</h1>
@@ -84,30 +87,28 @@ const faqs = [
           </p>
         </div>
       </div>
-      <div class="grid w-full h-full col-span-3 col-start-4 pl-12">
-        <div>
-          <div
-            class="gradient-circle rounded-full bg-gradient-to-br from-purple-400 via-pink-400 to-yellow-300"
-          >
-          <img class="pop-img relative object-contain" src="../assets/img/funkoPop_Flash.png" />
-        </div>
+      <!-- Right -->
+      <div class="hidden md:block grid w-full col-span-6 md:col-span-3 md:col-start-4 md:pl-12">
+        <div class="pop-container">
+          <div class="pop-circle bg-gradient-to-br from-purple-400 via-pink-400 to-yellow-300"></div>
+          <img class="pop-figure" src="../assets/img/funkoPop_Flash.png" />
         </div>
       </div>
 
     </div>
 
-    <div class="grid grid-cols-6 gap-8 place-content-center h-full">
-      <div class="grid w-full h-full col-span-3 col-start-1 pr-12">
-        <div class="gradient-circle w-95 h-95 rounded-full bg-gradient-to-br from-purple-400 via-pink-400 to-yellow-300">
-          <img
-          class="relative w-95 h-95 object-contain z-10 pop-img-obi"
-          src="../assets/img/funkoPop_Obi.png"
-          style="width: 27rem; height: 30rem"
-        />
+    <!-- Features -->
+    <div class="grid grid-cols-1 md:grid-cols-6 gap-8 place-content-center py-16 md:py-32">
+      <!-- Left -->
+      <div class="hidden md:block grid w-full col-span-6 md:col-span-3 md:col-start-1 md:pr-12">
+        <div class="pop-container">
+          <div class="pop-circle bg-gradient-to-br from-purple-400 via-pink-400 to-yellow-300"></div>
+          <img class="pop-figure-obi" src="../assets/img/funkoPop_Obi.png" />
         </div>
       </div>
-      <div class="grid w-full h-full col-span-3 col-start-4 pl-12">
-        <div class="justify-center items-center grid grid-cols-2 gap-8">
+      <!-- Right -->
+      <div class="grid w-full col-span-6 md:col-span-3 md:col-start-4 md:pl-12">
+        <div class="justify-center items-center grid grid-cols-1 md:grid-cols-2 gap-8">
           <div class="flex items-center justify-center flex-col">
             <i
             class="pi pi-box feature-icon bg-gradient-to-r from-purple-400 to-pink-600 text-transparent bg-clip-text"
@@ -140,16 +141,17 @@ const faqs = [
     </div>
 
     <!-- About -->
-    <div class="grid grid-cols-6 gap-8 place-content-center h-full">
+    <div class="grid grid-cols-1 md:grid-cols-6 gap-8 place-content-center py-16 md:py-32">
       <!-- Left Column-->
-      <div class="grid w-full h-full col-span-3 col-start-1 pr-12">
-        <h1 class="playfair-display-hometitle text-7xl landing-title font-bold">About</h1>
+      <div class="grid w-full col-span-6 md:col-span-3 md:col-start-1 md:pr-12">
+        <h1 class="playfair-display-hometitle text-4xl md:text-7xl landing-title font-bold">About</h1>
         <p class="text-2xl/12 home-text">{{ aboutText }}</p>
       </div>
 
       <!-- Right Column -->
-      <div class="grid w-full h-full col-span-3 col-start-4 pl-12">
-        <Carousel :value="images" :numVisible="1" :numScroll="1" circular>
+      <div class="grid w-full col-span-6 md:col-span-3 md:col-start-4 md:pl-12">
+        <div>
+          <Carousel :value="images" :numVisible="1" :numScroll="1" circular>
             <template #item="slotProps">
               <div class="flex flex-col items-center justify-center">
                 <img
@@ -162,12 +164,14 @@ const faqs = [
               </div>
             </template>
           </Carousel>
+        </div>
+
       </div>
 
     </div>
 
     <!-- Testimonials  -->
-    <div class="grid grid-cols-6 gap-8 place-content-center h-full">
+    <div class="grid grid-cols-6 gap-8 place-content-center py-32">
         <h1
         class="playfair-display-hometitle text-7xl landing-title font-bold grid col-span-6 col-start-1"
         style="display: grid; place-items: center;"
@@ -175,94 +179,198 @@ const faqs = [
         Testimonials
       </h1>
 
-      <div class="grid col-span-2 gap-4">
-        <Card style="width: 25rem; overflow: hidden">
-          <template #header>
-            <img alt="user header" src="/src/assets/img/funkoPop_Flash.png" />
-            <div class="flex justify-center gap-x-8">
-              <i v-for="n in 5" :key="n" class="pi pi-star-fill star"> </i>
-            </div>
-          </template>
-          <template #title>Advanced Card</template>
-          <template #subtitle>Card subtitle</template>
-          <template #content>
-            <p class="m-0">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
-              error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam
-              nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-            </p>
-          </template>
-          <template #footer> </template>
-        </Card>
-      </div>
+      <div class="col-span-6 grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center">
+        <div>
+          <Card class="w-full">
+            <template #header>
+              <img alt="user header" src="/src/assets/img/funkoPop_Flash.png" />
+              <div class="flex justify-center gap-x-8">
+                <i v-for="n in 5" :key="n" class="pi pi-star-fill star"> </i>
+              </div>
+            </template>
+            <template #title>Advanced Card</template>
+            <template #subtitle>Card subtitle</template>
+            <template #content>
+              <p class="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
+                error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam
+                nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+              </p>
+            </template>
+            <template #footer> </template>
+          </Card>
+        </div>
 
-      <div class="grid col-span-2 gap-4">
-        <Card style="width: 25rem; overflow: hidden">
-          <template #header>
-            <img alt="user header" src="/src/assets/img/funkoPop_Flash.png" />
-            <div class="flex justify-center gap-x-8">
-              <i v-for="n in 5" :key="n" class="pi pi-star-fill star"> </i>
-            </div>
-          </template>
-          <template #title>Advanced Card</template>
-          <template #subtitle>Card subtitle</template>
-          <template #content>
-            <p class="m-0">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
-              error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam
-              nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-            </p>
-          </template>
-          <template #footer> </template>
-        </Card>
-      </div>
+        <div>
+          <Card class="w-full">
+            <template #header>
+              <img alt="user header" src="/src/assets/img/funkoPop_Flash.png" />
+              <div class="flex justify-center gap-x-8">
+                <i v-for="n in 5" :key="n" class="pi pi-star-fill star"> </i>
+              </div>
+            </template>
+            <template #title>Advanced Card</template>
+            <template #subtitle>Card subtitle</template>
+            <template #content>
+              <p class="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
+                error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam
+                nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+              </p>
+            </template>
+            <template #footer> </template>
+          </Card>
+        </div>
 
-      <div class="grid col-span-2 gap-4">
-        <Card style="width: 25rem; overflow: hidden">
-          <template #header>
-            <img alt="user header" src="/src/assets/img/funkoPop_Flash.png" />
-            <div class="flex justify-center gap-x-8">
-              <i v-for="n in 5" :key="n" class="pi pi-star-fill star"> </i>
-            </div>
-          </template>
-          <template #title>Advanced Card</template>
-          <template #subtitle>Card subtitle</template>
-          <template #content>
-            <p class="m-0">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
-              error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam
-              nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-            </p>
-          </template>
-          <template #footer> </template>
-        </Card>
-      </div>
-
-      <!-- Left Column-->
-      <div class="grid w-full h-full col-span-3 col-start-1 pr-12">
-
-      </div>
-
-      <!-- Right Column -->
-      <div class="grid w-full h-full col-span-3 col-start-4 pl-12">
-
+        <div>
+          <Card class="w-full">
+            <template #header>
+              <img alt="user header" src="/src/assets/img/funkoPop_Flash.png" />
+              <div class="flex justify-center gap-x-8">
+                <i v-for="n in 5" :key="n" class="pi pi-star-fill star"> </i>
+              </div>
+            </template>
+            <template #title>Advanced Card</template>
+            <template #subtitle>Card subtitle</template>
+            <template #content>
+              <p class="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
+                error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam
+                nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+              </p>
+            </template>
+            <template #footer> </template>
+          </Card>
+        </div>
       </div>
 
     </div>
 
     <!-- FAQ -->
-    <div class="grid grid-cols-6 gap-8 place-content-center h-full">
-      <!-- Left Column-->
-      <div class="grid w-full h-full col-span-3 col-start-1 pr-12">
+    <div class="grid grid-cols-6 gap-8 place-content-center py-16 md:py-32">
+      <h1
+        class="playfair-display-hometitle text-7xl landing-title font-bold grid col-span-6 col-start-1"
+        style="display: grid; place-items: center;"
+      >
+        FAQs
+      </h1>
 
+      <div class="grid col-span-6 gap-4">
+      <Accordion>
+            <AccordionPanel v-for="(faq, i) in faqs" :key="i" :value="i" class="w-full flex-1">
+              <AccordionHeader>
+                <h1 class="faq-text-question">
+                  {{ faq.question }}
+                </h1>
+              </AccordionHeader>
+              <AccordionContent>
+                <p class="">
+                  {{ faq.answer }}
+                </p>
+              </AccordionContent>
+            </AccordionPanel>
+          </Accordion>
+      </div>
+    </div>
+
+    <!-- CTA -->
+    <div class="grid grid-cols-1 md:grid-cols-6 gap-8 place-content-center py-16 md:py-32">
+
+      <!-- Left Column-->
+      <div class="hidden md:block grid w-full col-span-1 md:col-span-3 md:col-start-1 pr-6 md:pr-12">
+        <div class="pop-container">
+          <div class="pop-circle bg-gradient-to-br from-purple-400 via-pink-400 to-yellow-300"></div>
+          <img class="pop-figure-obi" src="../assets/img/funkoPop_Obi.png" />
+        </div>
       </div>
 
       <!-- Right Column -->
-      <div class="grid w-full h-full col-span-3 col-start-4 pl-12">
-
+      <div class="grid w-full col-span-1 md:col-span-3 md:col-start-4 pl-6 md:pl-12">
+        <h1 class="playfair-display-hometitle text-4xl md:text-7xl landing-title font-bold">
+          Start Your Collection Today
+        </h1>
+        <p class="text-2xl/12 home-text">
+          Join collectors around the world who are organizing, tracking, and showcasing their Funko
+          Pop collections in one place.
+        </p>
+        <div class="auth-links w-fit" style="padding-top: 2rem">
+          <a @click="signInWithGoogle" class="login google-btn signin">Sign In with Google</a>
+        </div>
       </div>
 
     </div>
+
+    <!-- Footer -->
+     <footer>
+      <div class="grid grid-cols-1 md:grid-cols-4 gap-8 place-content-center py-16 md:py-32">
+        <div>
+          <p class="text-2xl/12 home-text">Funkollection</p>
+          <p>
+            Your one-stop app for organizing, tracking, and showcasing your Funko Pop collection.
+          </p>
+          <div class="flex gap-x-6 justify-start" style="padding-top: 2rem">
+              <a class="pi pi-github" href="/" target="_blank" rel="noopener noreferrer"></a>
+              <a class="pi pi-instagram" href="/" target="_blank" rel="noopener noreferrer"></a>
+              <a class="pi pi-tiktok" href="/" target="_blank" rel="noopener noreferrer"></a>
+              <a class="pi pi-youtube" href="/" target="_blank" rel="noopener noreferrer"></a>
+            </div>
+        </div>
+
+        <div>
+          <p class="text-2xl/12 home-text">Quick Links</p>
+          <div>
+              <a href="#home" class="footer-link">Home</a>
+            </div>
+
+            <div>
+              <a href="#features" class="footer-link">Features</a>
+            </div>
+
+            <div>
+              <a href="#about" class="footer-link">About</a>
+            </div>
+
+            <div>
+              <a href="#testimonials" class="footer-link">Testimonials</a>
+            </div>
+
+            <div>
+              <a href="#faq" class="footer-link">FAQs</a>
+            </div>
+        </div>
+
+        <div>
+           <p class="text-2xl/12 home-text">Company</p>
+           <div class="flex-col">
+            <div>
+              <a href="/" class="footer-link">About Us</a>
+            </div>
+
+            <div>
+              <a href="/" class="footer-link">Terms of Use</a>
+            </div>
+
+            <div>
+              <a href="/" class="footer-link">Privacy Policy</a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <p class="text-2xl/12 home-text">Get in Touch</p>
+          <p>
+              Have feedback or questions? Reach out via social media!
+            </p>
+        </div>
+
+      </div>
+      <Divider />
+      <div class="flex-row flex-1">
+        <p>@2025 Funkollection. All rights reserved.</p>
+      </div>
+    </footer>
+
+
 </main>
 
 
@@ -287,9 +395,17 @@ const faqs = [
   padding-right: 12rem;
   padding-top: 15rem;
   width: 100%; */
-  padding-top: 15rem;
-  padding-left: 12rem;
-  padding-right: 12rem;
+  padding-top: 8rem;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+}
+
+@media (min-width: 768px) {
+  .main-div {
+    padding-top: 15rem;
+    padding-left: 12rem;
+    padding-right: 12rem;
+  }
 }
 
 .landing-header {
@@ -310,14 +426,13 @@ const faqs = [
   color: #333;
 }
 .auth-links {
-  display: flex;
   gap: 1rem;
 }
 .auth-links a {
   text-decoration: none;
   color: #333;
   font-weight: 500;
-  padding: 0.5rem 1.2rem;
+  padding: 1rem;
   border-radius: 4px;
   transition: background 0.2s;
 }
@@ -357,13 +472,56 @@ const faqs = [
 }
 
 .gradient-circle {
-  width: 27rem;
-  height: 27rem;
+  width: 30rem;
+  height: 30rem;
 }
 
 .pop-img {
-  width: 37rem;
-  height: 37rem;
+  width: 42rem;
+  height: 42rem;
+  position: absolute;        /* Take it out of the circle's flow */
+  top: -4rem;
+  left: 50%;
+  transform: translateX(-50%); /* Center it horizontally */
+}
+
+.pop-container {
+  position: relative;
+  width: 20rem;
+  height: 20rem;
+}
+
+@media (min-width: 768px) {
+  .pop-container {
+    width: 40rem;
+    height: 40rem;
+  }
+}
+
+.pop-circle {
+  width: 70%;
+  height: 70%;
+  border-radius: 50%;
+}
+
+.pop-figure {
+  position: absolute;
+  width: 130%;          /* Bigger than the circle */
+  height: 130%;
+  object-fit: contain;
+  top: 50%;
+  left: 50%;
+  transform: translate(-65%, -62%);
+}
+
+.pop-figure-obi {
+  position: absolute;
+  width: 80%;          /* Bigger than the circle */
+  height: 75%;
+  object-fit: contain;
+  top: 50%;
+  left: 50%;
+  transform: translate(-68%, -68%);
 }
 
 .star {
